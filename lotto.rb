3b9e867a1141b4c3fbd56c161af5ce8340a2cd32
Fredrick Gorospe <gorospefredrick@gmail.com>
2020-10-20 17:39:27 +0800

    Changed amount of numbers from 45 to 10

--- lotto.rb
+++ lotto.rb
@@ -3,11 +3,11 @@
 lotto_ticket = []
 6.times do
   begin
-    print "Enter a number from 1 to 45: "
+    print "Enter a number from 1 to 10: "
     number = gets.to_i
-    raise RangeError if number < 1 || number > 45
+    raise RangeError if number < 1 || number > 10
   rescue RangeError 
-    puts "You must enter a number from 1 to 45"
+    puts "You must enter a number from 1 to 10"
     retry 
   end
   lotto_ticket << number 
@@ -15,7 +15,7 @@ end
 
 winning_numbers = []
 6.times do
-  winning_numbers << rand(1..45)
+  winning_numbers << rand(1..10)
 end
 
 puts "You bet: #{lotto_ticket}"
