7d6ff68e645908f2efa39940356fdc03751553d9
Fredrick Gorospe <gorospefredrick@gmail.com>
2020-10-20 17:36:56 +0800

    Fixed more bugs

--- lotto.rb
+++ lotto.rb
@@ -10,5 +10,16 @@ lotto_ticket = []
     puts "You must enter a number from 1 to 45"
     retry 
   end
+  lotto_ticket << number 
 end
-puts lotto_ticket
+
+winning_numbers = []
+6.times do
+  winning_numbers << rand(1..45)
+end
+
+puts "You bet: #{lotto_ticket}"
+puts
+puts "The winning are: #{winning_numbers}"
+
+
